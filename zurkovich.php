a98ed17bc49e7ec4fb60ab311072acf114ef285c
transatlanticvoyage <sandbagexpert@gmail.com>
2025-05-20 17:58:36 -0400

    1

--- zurkovich.php
+++ zurkovich.php
@@ -13,27 +13,107 @@ if (!defined('ABSPATH')) {
 
 // Add admin menu
 function zurkovich_admin_menu() {
+    // Add main menu item
     add_menu_page(
-        'Zurkovich',
-        'Zurkovich',
-        'manage_options',
-        'zurkoscreen1',
-        'zurkovich_screen1',
-        'dashicons-admin-generic',
+        'Zurkovich', // Page title
+        'Zurkovich', // Menu title
+        'manage_options', // Capability
+        'zurkovich', // Menu slug
+        'zurkovich_screen1', // Function to display the page
+        'dashicons-admin-generic', // Icon
         2 // Position between Dashboard and Posts
     );
 
     // Add submenu pages
-    add_submenu_page('zurkoscreen1', 'Screen 1', 'Screen 1', 'manage_options', 'zurkoscreen1', 'zurkovich_screen1');
-    add_submenu_page('zurkoscreen1', 'Screen 2', 'Screen 2', 'manage_options', 'zurkoscreen2', 'zurkovich_screen2');
-    add_submenu_page('zurkoscreen1', 'Screen 3', 'Screen 3', 'manage_options', 'zurkoscreen3', 'zurkovich_screen3');
-    add_submenu_page('zurkoscreen1', 'Screen 4', 'Screen 4', 'manage_options', 'zurkoscreen4', 'zurkovich_screen4');
-    add_submenu_page('zurkoscreen1', 'Screen 5', 'Screen 5', 'manage_options', 'zurkoscreen5', 'zurkovich_screen5');
-    add_submenu_page('zurkoscreen1', 'Screen 6', 'Screen 6', 'manage_options', 'zurkoscreen6', 'zurkovich_screen6');
-    add_submenu_page('zurkoscreen1', 'Screen 7', 'Screen 7', 'manage_options', 'zurkoscreen7', 'zurkovich_screen7');
-    add_submenu_page('zurkoscreen1', 'Screen 8', 'Screen 8', 'manage_options', 'zurkoscreen8', 'zurkovich_screen8');
-    add_submenu_page('zurkoscreen1', 'Screen 9', 'Screen 9', 'manage_options', 'zurkoscreen9', 'zurkovich_screen9');
-    add_submenu_page('zurkoscreen1', 'Screen 10', 'Screen 10', 'manage_options', 'zurkoscreen10', 'zurkovich_screen10');
+    add_submenu_page(
+        'zurkovich', // Parent slug
+        'Screen 1', // Page title
+        'Screen 1', // Menu title
+        'manage_options', // Capability
+        'zurkovich', // Menu slug (same as parent for first item)
+        'zurkovich_screen1' // Function to display the page
+    );
+
+    add_submenu_page(
+        'zurkovich', // Parent slug
+        'Screen 2', // Page title
+        'Screen 2', // Menu title
+        'manage_options', // Capability
+        'zurkovich-screen2', // Menu slug
+        'zurkovich_screen2' // Function to display the page
+    );
+
+    add_submenu_page(
+        'zurkovich',
+        'Screen 3',
+        'Screen 3',
+        'manage_options',
+        'zurkovich-screen3',
+        'zurkovich_screen3'
+    );
+
+    add_submenu_page(
+        'zurkovich',
+        'Screen 4',
+        'Screen 4',
+        'manage_options',
+        'zurkovich-screen4',
+        'zurkovich_screen4'
+    );
+
+    add_submenu_page(
+        'zurkovich',
+        'Screen 5',
+        'Screen 5',
+        'manage_options',
+        'zurkovich-screen5',
+        'zurkovich_screen5'
+    );
+
+    add_submenu_page(
+        'zurkovich',
+        'Screen 6',
+        'Screen 6',
+        'manage_options',
+        'zurkovich-screen6',
+        'zurkovich_screen6'
+    );
+
+    add_submenu_page(
+        'zurkovich',
+        'Screen 7',
+        'Screen 7',
+        'manage_options',
+        'zurkovich-screen7',
+        'zurkovich_screen7'
+    );
+
+    add_submenu_page(
+        'zurkovich',
+        'Screen 8',
+        'Screen 8',
+        'manage_options',
+        'zurkovich-screen8',
+        'zurkovich_screen8'
+    );
+
+    add_submenu_page(
+        'zurkovich',
+        'Screen 9',
+        'Screen 9',
+        'manage_options',
+        'zurkovich-screen9',
+        'zurkovich_screen9'
+    );
+
+    add_submenu_page(
+        'zurkovich',
+        'Screen 10',
+        'Screen 10',
+        'manage_options',
+        'zurkovich-screen10',
+        'zurkovich_screen10'
+    );
 }
 add_action('admin_menu', 'zurkovich_admin_menu');
 
