d0207ce620054ec3bfb9d2a1b9192a90a063227e
transatlanticvoyage <sandbagexpert@gmail.com>
2025-05-22 14:44:18 -0400

    3

--- includes/file-function-inject-content-2.php
+++ includes/file-function-inject-content-2.php
@@ -42,26 +42,34 @@ function function_inject_content_2($page_id, $zeeprex_content) {
     // Save mapping meta
     update_post_meta($page_id, 'zeeprex_map', $map);
 
-    // Get Elementor document
-    $document = \Elementor\Plugin::$instance->documents->get($page_id);
-    if (!$document) {
+    // Get Elementor data
+    $data = get_post_meta($page_id, '_elementor_data', true);
+    if (!$data) {
         return false;
     }
 
-    // Get the data
-    $data = $document->get_elements_data();
-    if (empty($data)) {
+    // Decode the data
+    $elements = json_decode($data, true);
+    if (!is_array($elements)) {
         return false;
     }
 
-    // Process the data
-    $data = process_elements($data, $map);
+    // Process the elements
+    $elements = process_elements($elements, $map);
 
-    // Update the document
-    $document->save([
-        'elements' => $data,
-        'settings' => $document->get_settings()
-    ]);
+    // Save the updated data
+    $updated_data = wp_json_encode($elements);
+    if ($updated_data === false) {
+        return false;
+    }
+
+    // Update the post meta
+    update_post_meta($page_id, '_elementor_data', $updated_data);
+
+    // Clear Elementor cache
+    if (class_exists('\Elementor\Plugin')) {
+        \Elementor\Plugin::$instance->files_manager->clear_cache();
+    }
 
     return true;
 }
@@ -72,16 +80,69 @@ function function_inject_content_2($page_id, $zeeprex_content) {
 function process_elements($elements, $map) {
     foreach ($elements as &$el) {
         if (isset($el['settings']) && is_array($el['settings'])) {
-            foreach ($el['settings'] as $skey => $sval) {
-                if (is_string($sval)) {
-                    foreach ($map as $key => $val) {
-                        if ($sval === $key) {
-                            $el['settings'][$skey] = $val;
+            // Store original settings
+            $original_settings = $el['settings'];
+            
+            // Update content fields based on widget type
+            if (isset($el['widgetType'])) {
+                switch ($el['widgetType']) {
+                    case 'heading':
+                        if (isset($original_settings['title'])) {
+                            foreach ($map as $key => $val) {
+                                if ($original_settings['title'] === $key) {
+                                    $el['settings']['title'] = $val;
+                                    // Preserve typography settings
+                                    if (isset($original_settings['typography_typography'])) {
+                                        $el['settings']['typography_typography'] = $original_settings['typography_typography'];
+                                    }
+                                }
+                            }
                         }
-                    }
+                        break;
+                        
+                    case 'text-editor':
+                        if (isset($original_settings['editor'])) {
+                            foreach ($map as $key => $val) {
+                                if ($original_settings['editor'] === $key) {
+                                    $el['settings']['editor'] = $val;
+                                    // Preserve typography settings
+                                    if (isset($original_settings['typography_typography'])) {
+                                        $el['settings']['typography_typography'] = $original_settings['typography_typography'];
+                                    }
+                                }
+                            }
+                        }
+                        break;
+                        
+                    case 'image-box':
+                        if (isset($original_settings['title_text'])) {
+                            foreach ($map as $key => $val) {
+                                if ($original_settings['title_text'] === $key) {
+                                    $el['settings']['title_text'] = $val;
+                                    // Preserve title typography
+                                    if (isset($original_settings['title_typography_typography'])) {
+                                        $el['settings']['title_typography_typography'] = $original_settings['title_typography_typography'];
+                                    }
+                                }
+                            }
+                        }
+                        if (isset($original_settings['description_text'])) {
+                            foreach ($map as $key => $val) {
+                                if ($original_settings['description_text'] === $key) {
+                                    $el['settings']['description_text'] = $val;
+                                    // Preserve description typography
+                                    if (isset($original_settings['description_typography_typography'])) {
+                                        $el['settings']['description_typography_typography'] = $original_settings['description_typography_typography'];
+                                    }
+                                }
+                            }
+                        }
+                        break;
                 }
             }
         }
+        
+        // Process child elements
         if (isset($el['elements']) && is_array($el['elements'])) {
             $el['elements'] = process_elements($el['elements'], $map);
         }
