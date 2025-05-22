bba9bb5f85062436129311d7803367359fbc4c76
transatlanticvoyage <sandbagexpert@gmail.com>
2025-05-22 14:45:43 -0400

    4

--- includes/file-function-inject-content-2.php
+++ includes/file-function-inject-content-2.php
@@ -11,7 +11,10 @@ if (!defined('ABSPATH')) {
  * @return bool True on success, false on failure.
  */
 function function_inject_content_2($page_id, $zeeprex_content) {
+    error_log('Starting function_inject_content_2 for page_id: ' . $page_id);
+    
     if (empty($zeeprex_content)) {
+        error_log('Empty zeeprex_content');
         return false;
     }
     
@@ -25,9 +28,11 @@ function function_inject_content_2($page_id, $zeeprex_content) {
         if (preg_match('/^>y_([^\s]+)/', $line, $m)) {
             $key = 'y_' . $m[1];
             $map[$key] = '';
+            error_log('Found y_ code: ' . $key);
         } elseif (preg_match('/^>Y_([^\s]+)/', $line, $m)) {
             $key = 'Y_' . $m[1];
             $map[$key] = '';
+            error_log('Found Y_ code: ' . $key);
         } elseif (preg_match('/^>/', $line)) {
             $key = '';
         } elseif ($key !== '') {
@@ -36,21 +41,30 @@ function function_inject_content_2($page_id, $zeeprex_content) {
     }
     
     if (empty($map)) {
+        error_log('No codes found in content');
         return false;
     }
 
+    error_log('Found ' . count($map) . ' codes to process');
+    error_log('Codes: ' . implode(', ', array_keys($map)));
+
     // Save mapping meta
     update_post_meta($page_id, 'zeeprex_map', $map);
+    error_log('Saved mapping to zeeprex_map');
 
     // Get Elementor data
     $data = get_post_meta($page_id, '_elementor_data', true);
     if (!$data) {
+        error_log('No Elementor data found for page');
         return false;
     }
+
+    error_log('Raw Elementor data: ' . substr($data, 0, 200) . '...');
 
     // Decode the data
     $elements = json_decode($data, true);
     if (!is_array($elements)) {
+        error_log('Failed to decode Elementor data');
         return false;
     }
 
@@ -60,15 +74,20 @@ function function_inject_content_2($page_id, $zeeprex_content) {
     // Save the updated data
     $updated_data = wp_json_encode($elements);
     if ($updated_data === false) {
+        error_log('Failed to encode updated data');
         return false;
     }
 
+    error_log('Updated Elementor data: ' . substr($updated_data, 0, 200) . '...');
+
     // Update the post meta
-    update_post_meta($page_id, '_elementor_data', $updated_data);
+    $update_result = update_post_meta($page_id, '_elementor_data', $updated_data);
+    error_log('Update post meta result: ' . ($update_result ? 'success' : 'failed'));
 
     // Clear Elementor cache
     if (class_exists('\Elementor\Plugin')) {
         \Elementor\Plugin::$instance->files_manager->clear_cache();
+        error_log('Cleared Elementor cache');
     }
 
     return true;
@@ -85,11 +104,14 @@ function process_elements($elements, $map) {
             
             // Update content fields based on widget type
             if (isset($el['widgetType'])) {
+                error_log('Processing widget type: ' . $el['widgetType']);
+                
                 switch ($el['widgetType']) {
                     case 'heading':
                         if (isset($original_settings['title'])) {
                             foreach ($map as $key => $val) {
                                 if ($original_settings['title'] === $key) {
+                                    error_log('Found match in heading title: ' . $key);
                                     $el['settings']['title'] = $val;
                                     // Preserve typography settings
                                     if (isset($original_settings['typography_typography'])) {
@@ -104,6 +126,7 @@ function process_elements($elements, $map) {
                         if (isset($original_settings['editor'])) {
                             foreach ($map as $key => $val) {
                                 if ($original_settings['editor'] === $key) {
+                                    error_log('Found match in text-editor editor: ' . $key);
                                     $el['settings']['editor'] = $val;
                                     // Preserve typography settings
                                     if (isset($original_settings['typography_typography'])) {
@@ -118,6 +141,7 @@ function process_elements($elements, $map) {
                         if (isset($original_settings['title_text'])) {
                             foreach ($map as $key => $val) {
                                 if ($original_settings['title_text'] === $key) {
+                                    error_log('Found match in image-box title_text: ' . $key);
                                     $el['settings']['title_text'] = $val;
                                     // Preserve title typography
                                     if (isset($original_settings['title_typography_typography'])) {
@@ -129,6 +153,7 @@ function process_elements($elements, $map) {
                         if (isset($original_settings['description_text'])) {
                             foreach ($map as $key => $val) {
                                 if ($original_settings['description_text'] === $key) {
+                                    error_log('Found match in image-box description_text: ' . $key);
                                     $el['settings']['description_text'] = $val;
                                     // Preserve description typography
                                     if (isset($original_settings['description_typography_typography'])) {
